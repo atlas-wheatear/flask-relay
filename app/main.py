5f2daee9c075e20d7a52ef9aa147cae9a5883767
Edward Moulsdale <eam152@student.aru.ac.uk>
2021-05-06 09:13:52 +0100

    Removed docker compose, improved security posture

--- app/main.py
+++ app/main.py
@@ -1,22 +1,33 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, send_file
+from werkzeug.utils import secure_filename
 from uuid import uuid4
 
 UPLOAD_DIR = "/uploads/"
-SUFFIX = ".ogg"
+EXTENSION = "ogg"
+SUFFIX = f".{EXTENSION}"
 
 app = Flask(__name__)
 
 # ban too large uploads
 app.config['MAX_CONTENT_LENGTH'] = 1024 * 1024
 
+
+def get_filename(uuid: str):
+    secure = secure_filename(uuid)
+    return f"{UPLOAD_DIR}{secure}{SUFFIX}"
+
 @app.route('/', methods=['POST'])
 def upload_ogg():
-    ogg_file = request.files["ogg"]
-    hex = uuid4().hex
-    filename = UPLOAD_DIR + hex + SUFFIX
+    ogg_file = request.files[EXTENSION]
+    uuid = uuid4().hex
+    filename = get_filename(uuid)
     ogg_file.save(filename)
     return jsonify({
         "uuid": hex
     })
 
-
+@app.route("/dl", methods=['GET'])
+def get_ogg():
+    uuid = request.json["uuid"]
+    filename = get_filename(uuid)
+    return send_file(filename)
